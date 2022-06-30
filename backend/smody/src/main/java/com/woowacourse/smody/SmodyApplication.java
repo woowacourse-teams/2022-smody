aa70228c036de5675772e1b5914af9eb76c1ce4e
tonic523 <diddmlvkf@gmail.com>
2022-06-30 14:41:22 +0900

    feat: spring 초기 설정

---
+++ backend/smody/src/main/java/com/woowacourse/smody/SmodyApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.smody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmodyApplication.class, args);
+    }
+
+}
