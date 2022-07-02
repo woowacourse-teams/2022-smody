70de0e9d632744856dc1f29d65d1e7415f89d4e4
woose28 <dldntmdsla@naver.com>
2022-07-02 12:12:16 +0900

    chore: 필요한 패키지 설치
    - storybook
    - react router dom
    
    Co-authored-by: Marco <wonsss@users.noreply.github.com>
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,26 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended", "plugin:storybook/recommended"],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": ["react", "@typescript-eslint"],
+  "rules": {}
+};
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,28 +1,29 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const webpack = require("webpack");
-const prod = process.env.NODE_ENV === "production";
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: prod ? "production" : "development",
-  devtool: prod ? "hidden-source-map" : "eval",
+  mode: prod ? 'production' : 'development',
+  devtool: prod ? 'hidden-source-map' : 'eval',
   performance: {
     hints: false,
   },
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.[chunkhash].js",
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.[chunkhash].js',
     clean: true,
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'], // node_modules/보다 우선으로 검색할 디렉터리를 추가하는 것은 다음과 같습니다.
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: ["babel-loader", "ts-loader"],
+        use: ['babel-loader', 'ts-loader'],
       },
     ],
   },
@@ -34,15 +35,15 @@ module.exports = {
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
   },
   plugins: [
     new webpack.ProvidePlugin({
-      React: "react",
+      React: 'react',
     }),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
