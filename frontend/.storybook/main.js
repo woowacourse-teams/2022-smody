9e5740f46d89710b6d2b0c13d1e721deeeb51ed0
woose28 <dldntmdsla@naver.com>
2022-07-02 17:55:12 +0900

    fix: storybook 절대 경로 이슈 해결
    Co-authored-by: ronci <ronci@users.noreply.github.com>
    Co-authored-by: Marco <wonsss@users.noreply.github.com>

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,15 +1,23 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    // '../src'는 현재 파일 위치에서 baseUrl로 설정한 위치까지의 상대 경로가 들어간다.
+    config.resolve.modules = [
+      ...config.resolve.modules,
+      path.resolve(__dirname, '../src'),
+    ];
+
+    return config;
+  },
+};
--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,9 +1,26 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from 'styles/GlobalStyle';
+import { lightTheme } from 'styles/theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    </ThemeProvider>
+  ),
+];
