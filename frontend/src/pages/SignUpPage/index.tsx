c55b5bfaec00ef26941b1a7bb22e661587dc6043
woose28 <dldntmdsla@naver.com>
2022-07-03 14:41:10 +0900

    feat: SingUp 페이지 초안 작성
    Co-authored-by: ronci <ronci@users.noreply.github.com>
    Co-authored-by: Marco <wonsss@users.noreply.github.com>

--- frontend/src/pages/SignUpPage/index.tsx
+++ frontend/src/pages/SignUpPage/index.tsx
@@ -1,5 +1,35 @@
+import styled from 'styled-components';
+
+import { Text } from 'components/@shared/Text';
+
+import { Input } from 'components/Input';
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <Container>
+      {/* <img src={ } alt='로고' width='100px' height='60px' /> */}
+
+      <Text size={24} color="#000000">
+        회원가입
+      </Text>
+      <Input
+        label="이메일"
+        type="text"
+        value=""
+        onChange={(e) => {}}
+        placeholder="이것은 이메일"
+        message=""
+      />
+      {/* <Input />
+      <Input />
+      <Input />
+
+      <Button />
+      <Text /> */}
+    </Container>
+  );
 };
+
+const Container = styled.main``;
 
 export default SignUpPage;
