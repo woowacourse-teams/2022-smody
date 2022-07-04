be480a8bc59ad9cc2da31b68593def6ae50d45f9
woose28 <dldntmdsla@naver.com>
2022-07-04 21:00:17 +0900

    chore: eslint-plugin-prettier 설치 및 설정
    Co-authored-by: Marco <wonsss@users.noreply.github.com>
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended", "plugin:storybook/recommended"],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  extends: [
+    'prettier',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  "plugins": ["react", "@typescript-eslint"],
-  "rules": {}
+  plugins: ['prettier', 'react', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
 };
