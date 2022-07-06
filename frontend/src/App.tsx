fbe58eddb21896eca10c00d2c59a70c068ecad2a
woose28 <dldntmdsla@naver.com>
2022-07-06 22:29:43 +0900

    faet: react query 설정
    Co-authored-by: ronci <ronci@users.noreply.github.com>
    Co-authored-by: Marco <wonsss@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
 import Router from 'Router';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from 'styles/GlobalStyle';
 import { lightTheme } from 'styles/theme';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
+        <Router />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
