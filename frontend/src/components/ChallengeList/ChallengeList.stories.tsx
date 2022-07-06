40afdb4f14f9adc3c2b4aa88821d5b6a6ec02483
woose28 <dldntmdsla@naver.com>
2022-07-06 14:02:49 +0900

    fix: ChallengeList storybook 수정
    Co-authored-by: ronci <ronci@users.noreply.github.com>
    Co-authored-by: Marco <wonsss@users.noreply.github.com>

--- frontend/src/components/ChallengeList/ChallengeList.stories.tsx
+++ frontend/src/components/ChallengeList/ChallengeList.stories.tsx
@@ -1,16 +1,8 @@
 import { ChallengeList } from 'components';
-import { ChallengeListProps } from 'components/ChallengeList/type';
 
 export default {
   title: 'Components/ChallengeList',
   component: ChallengeList,
 };
 
-export const DefaultChallengeList = (args: ChallengeListProps) => (
-  <ChallengeList {...args} />
-);
-
-DefaultChallengeList.args = {
-  challengeName: '미라클모닝',
-  challengerCount: 15,
-};
+export const DefaultChallengeList = () => <ChallengeList />;
