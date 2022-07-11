6995c60085921e34536987c4e6a3f8490577a40c
Marco <bigdatajang@gmail.com>
2022-07-11 18:39:08 +0900

    feat: 인증 완료 후 남은 시간 표시 방법 변경
    setInterval 리팩토링
    
    ex)
    - 24시간 미만
        - 23시간 남았습니다
        - 59분 남았습니다
    - 24시간 이상
        - 3시간 후부터 인증할 수 있습니다
        - 59분 후부터 인증할 수 있습니다
    
    Co-authored-by: ronci <ronci@users.noreply.github.com>
    Co-authored-by: Woo seung <woose28@users.noreply.github.com>

--- frontend/src/components/Timer/index.tsx
+++ frontend/src/components/Timer/index.tsx
@@ -5,42 +5,56 @@ import { ThemeContext } from 'styled-components';
 import { FlexBox, Text } from 'components';
 import { TimerProps } from 'components/Timer/type';
 
-const inMillisecond = 1000;
-const inMinutes = 60;
-const inHours = 3600;
+import { TIME_UPDATE_MS_PERIOD } from 'constants/domain';
+
+const getLeftTimeMessage = (certEndDate: Date) => {
+  const inMillisecond = 1000;
+  const inMinutes = 60;
+  const inHours = 3600;
+  const inDays = 86400;
+
+  const endTime = certEndDate.getTime();
+  const nowTime = new Date().getTime();
+
+  let remainSeconds = (endTime - nowTime) / inMillisecond;
+  let closingMessage = '남았습니다';
+
+  if (remainSeconds >= inDays) {
+    closingMessage = '후부터 인증할 수 있습니다';
+    remainSeconds -= inDays;
+  }
+
+  if (remainSeconds >= inHours) {
+    return Math.floor(remainSeconds / inHours) + '시간 ' + closingMessage;
+  }
+
+  if (remainSeconds >= inMinutes) {
+    return Math.floor(remainSeconds / inMinutes) + '분 ' + closingMessage;
+  }
+  return '1분 미만 ' + closingMessage;
+};
 
 export const Timer = ({ certEndDate }: TimerProps) => {
+  const [message, setMessage] = useState(() => getLeftTimeMessage(certEndDate));
   const themeContext = useContext(ThemeContext);
-  const [_, setToggle] = useState(false);
+  const timerColor = themeContext.onSurface;
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setToggle((prev) => !prev);
-    }, inMillisecond * inMinutes);
+      const newMessage = getLeftTimeMessage(certEndDate);
+      if (newMessage !== message) {
+        setMessage(newMessage);
+      }
+    }, TIME_UPDATE_MS_PERIOD);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  const calculateRemainTime = (certEndDate: Date) => {
-    const endTime = certEndDate.getTime();
-    const nowTime = new Date().getTime();
-
-    const remainSeconds = (endTime - nowTime) / 1000;
-
-    if (remainSeconds >= inHours) {
-      return Math.floor(remainSeconds / inHours) + '시간 남았습니다';
-    }
-    if (remainSeconds >= inMinutes) {
-      return Math.floor(remainSeconds / inMinutes) + '분 남았습니다';
-    }
-    return '1분 미만 남았습니다.';
-  };
-
   return (
     <FlexBox>
-      <BiAlarm />
-      <Text color={themeContext.onSurface} size={20}>
-        {calculateRemainTime(certEndDate)}
+      <BiAlarm color={timerColor} />
+      <Text color={timerColor} size={20}>
+        {message}
       </Text>
     </FlexBox>
   );
