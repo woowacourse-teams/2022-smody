aab5ed62b920c599114e0d8078fcfb8c9eb5cccd
Marco <bigdatajang@gmail.com>
2022-07-13 16:30:47 +0900

    feat: Header 컴포넌트 UI 변경
    path에 따라 헤더 색상 변경 기능 추가
    
    Co-authored-by: ronci <ronci@users.noreply.github.com>
    Co-authored-by: Woo seung <woose28@users.noreply.github.com>

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,41 +1,40 @@
-import { authApiClient } from 'apis/apiClient';
 import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilValue, useRecoilState } from 'recoil';
-import { nicknameState, isLoginState } from 'recoil/auth/atoms';
+import { FaBell } from 'react-icons/fa';
+import { useLocation } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { isLoginState } from 'recoil/auth/atoms';
 import styled, { ThemeContext, css } from 'styled-components';
 
-import { Logo, FlexBox, LinkText, Text } from 'components';
+import { Logo, FlexBox, LinkText } from 'components';
+import { WrapperProps } from 'components/Header/type';
 
 import { CLIENT_PATH } from 'constants/path';
 
 export const Header = () => {
-  const [isLogin, setIsLogin] = useRecoilState(isLoginState);
-  const nickname = useRecoilValue(nicknameState);
+  const isLogin = useRecoilValue(isLoginState);
   const themeContext = useContext(ThemeContext);
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    authApiClient.deleteAuth();
-    setIsLogin(false);
-    navigate(CLIENT_PATH.HOME);
+  const { pathname } = useLocation();
+
+  const pathMatchRoute = (routes: string[]) => {
+    let matchResultColor = themeContext.background;
+    routes.forEach((route) => {
+      if (pathname.includes(route)) {
+        matchResultColor = themeContext.secondary;
+        return;
+      }
+    });
+    return matchResultColor;
   };
 
+  const bgColor = pathMatchRoute([CLIENT_PATH.CERT]);
+
   return (
-    <Wrapper>
-      <Logo width="100" color={themeContext.onPrimary} />
+    <Wrapper bgColor={bgColor}>
+      <Logo width="100" color={themeContext.primary} />
       {isLogin ? (
-        <UserWrapper>
-          <Text size={20} color={themeContext.onPrimary}>
-            {nickname}
-          </Text>
-          <button onClick={handleLogout}>
-            <Text size={20} color={themeContext.onPrimary}>
-              로그아웃
-            </Text>
-          </button>
-        </UserWrapper>
+        <FaBell size={20} color={themeContext.primary} />
       ) : (
-        <LinkText to="/login" size={20} color={themeContext.onPrimary}>
+        <LinkText to="/login" size={20} color={themeContext.primary}>
           로그인
         </LinkText>
       )}
@@ -46,21 +45,14 @@ export const Header = () => {
 const Wrapper = styled(FlexBox).attrs({
   justifyContent: 'space-between',
   alignItems: 'center',
-})`
-  ${({ theme }) => css`
+})<WrapperProps>`
+  ${({ bgColor }) => css`
     position: fixed;
     top: 0;
     left: 0;
     right: 0;
-    background-color: ${theme.primary};
+    background-color: ${bgColor};
     padding: 1.2rem 4rem;
     z-index: 1000;
   `}
-`;
-
-const UserWrapper = styled(FlexBox).attrs({
-  alignItems: 'center',
-  gap: '1rem',
-})`
-  width: fit-content;
 `;
