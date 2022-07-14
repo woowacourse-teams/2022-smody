2566ddd0d5b6954ba3b45caf30c3ce782fb62122
jojogreen91 <iawbg13@gmail.com>
2022-07-14 14:20:06 +0900

    refactor: oauth 프로덕션 코드 리팩토링

--- backend/smody/src/main/java/com/woowacourse/smody/dto/GoogleTokenResponse.java
+++ backend/smody/src/main/java/com/woowacourse/smody/dto/GoogleTokenResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class GoogleOauthResponse {
+public class GoogleTokenResponse {
 
     private String access_token;
     private String scope;
--- backend/smody/src/main/java/com/woowacourse/smody/exception/ExceptionData.java
+++ backend/smody/src/main/java/com/woowacourse/smody/exception/ExceptionData.java
@@ -16,6 +16,7 @@ public enum ExceptionData {
     INVALID_LOGIN(2001, "이메일 혹은 비밀번호가 일치하지 않습니다.", 401),
     INVALID_TOKEN(2002, "유효하지 않은 토큰입니다.", 403),
     UNAUTHORIZED_MEMBER(2003, "인가되지 않은 회원입니다.", 403),
+    INVALID_AUTHORIZATION_CODE(2004, "OAuth 인증 코드가 올바르지 않습니다.", 403),
 
     ALREADY_SUCCESS(3001, "이미 성공한 사이클입니다.", 400),
     INVALID_PROGRESS_TIME(3002, "인증할 수 있는 시간이 아닙니다.", 400),
@@ -25,8 +26,8 @@ public enum ExceptionData {
     NOT_FOUND_MEMBER(4001, "존재하지 않는 회원입니다.", 404),
     NOT_FOUND_CHALLENGE(4002, "존재하지 않는 챌린지입니다.", 404),
     NOT_FOUND_CYCLE(4003, "존재하지 않는 사이클입니다.", 404),
-
     AUTHORIZATION_SERVER_ERROR(9001, "인가 관련 서버 내부의 오류입니다.", 500);
+
 
     private final int code;
     private final String message;
