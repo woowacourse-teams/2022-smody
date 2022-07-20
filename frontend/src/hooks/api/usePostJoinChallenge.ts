da935317e53abe3ff53772d8a9399905c5f6f2e0
Marco <bigdatajang@gmail.com>
2022-07-20 17:44:47 +0900

    fix: 에러 발생 시 챌린지이름 전달
    Co-authored-by: Woo seung <woose28@users.noreply.github.com>

--- frontend/src/hooks/api/usePostJoinChallenge.ts
+++ frontend/src/hooks/api/usePostJoinChallenge.ts
@@ -1,4 +1,5 @@
 import { usePostCycle } from 'apis';
+import { useRef } from 'react';
 
 import { PostJoinChallengeProps } from 'hooks/api/type';
 import useSnackBar from 'hooks/useSnackBar';
@@ -7,9 +8,10 @@ import { CLIENT_PATH } from 'constants/path';
 
 const usePostJoinChallenge = ({
   challengeId,
-  challengeName,
   challengeListRefetch,
 }: PostJoinChallengeProps) => {
+  const challengeNameRef = useRef('');
+
   const renderSnackBar = useSnackBar();
 
   const { mutate, isSuccess } = usePostCycle({
@@ -17,7 +19,7 @@ const usePostJoinChallenge = ({
       challengeListRefetch && challengeListRefetch();
 
       renderSnackBar({
-        message: `${challengeName} 챌린지에 성공적으로 참여하였습니다`,
+        message: `${challengeNameRef.current} 챌린지에 성공적으로 참여하였습니다`,
         status: 'SUCCESS',
         linkText: '인증하러 가기',
         linkTo: CLIENT_PATH.CERT,
@@ -25,7 +27,7 @@ const usePostJoinChallenge = ({
     },
     onError: () => {
       renderSnackBar({
-        message: `${challengeName} 챌린지 참여 시 에러가 발생했습니다`,
+        message: `${challengeNameRef.current} 챌린지 참여 시 에러가 발생했습니다`,
         status: 'ERROR',
         linkText: '문의하기',
         linkTo: CLIENT_PATH.VOC,
@@ -33,7 +35,8 @@ const usePostJoinChallenge = ({
     },
   });
 
-  const joinChallenge = () => {
+  const joinChallenge = (challengeName: string) => {
+    challengeNameRef.current = challengeName;
     mutate({ challengeId });
   };
 
