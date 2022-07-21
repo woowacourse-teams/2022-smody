892ca9ec40ec9e3d4d488b936918200f9e2d6aef
jojogreen91 <iawbg13@gmail.com>
2022-07-21 10:16:53 +0900

    6

--- backend/smody/src/main/java/com/woowacourse/smody/controller/webhook/WebHookController.java
+++ backend/smody/src/main/java/com/woowacourse/smody/controller/webhook/WebHookController.java
@@ -1,9 +1,10 @@
-package com.woowacourse.smody.controller;
+package com.woowacourse.smody.controller.webhook;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebHookController {
 
     @PostMapping("/deploy")
-    public ResponseEntity<String> deploy(@RequestBody String body) {
+    public ResponseEntity<Void> deploy(@RequestHeader(value = "X-GitHub-Hook-ID") String gitHubHookId,
+                                       @RequestBody GitHubHookRequest gitHubHookRequest) {
+        if (!gitHubHookId.equals("369627662") || !gitHubHookRequest.getRef().equals("CD")) {
+            return ResponseEntity.noContent().build();
+        }
+
         try {
             Runtime.getRuntime().exec("/home/ubuntu/spring-deploy.sh");
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/isReal")
     public String isReal() {
-        return "이게 된다고? 진짜? 레알? 이거 맞음???? 우석아 안녕, 나 이거 우리집에서 푸쉬함 ㅎ";
+        return "이게 된다고? 진짜? 레알? 이거맞음???? 우석아 안녕, 나 이거 우리집에서 푸쉬함 ㅎ";
     }
 }
