1388620eb2e4d338b46e6f95f17e408c09f705ad
2019015414 <2019015414@hanyang.ac.kr>
2022-07-21 21:53:10 +0900

    feat: 간편 로그인에 실패했을 때 스낵바 추가

--- frontend/src/hooks/useGoogleLogin.ts
+++ frontend/src/hooks/useGoogleLogin.ts
@@ -1,12 +1,25 @@
 import { useGetLinkGoogle } from 'apis';
 
+import useSnackBar from 'hooks/useSnackBar';
+
+import { CLIENT_PATH } from 'constants/path';
+
 export const useGoogleLogin = () => {
+  const renderSnackBar = useSnackBar();
   const { refetch: getLinkGoogle } = useGetLinkGoogle({
     enabled: false,
     onSuccess: ({ data: redirectionUrl }) => {
       window.location.href = redirectionUrl;
     },
+    onError: () => {
+      renderSnackBar({
+        message: '간편 로그인에 문제가 발생했습니다',
+        status: 'ERROR',
+        linkText: '문의하기',
+        linkTo: CLIENT_PATH.VOC,
+      });
+    },
   });
 
-  return getLinkGoogle;
+  return () => getLinkGoogle();
 };
