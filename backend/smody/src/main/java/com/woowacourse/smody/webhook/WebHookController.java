2c39c78adde6c825d282d04e3b306b229f365dda
jojogreen91 <iawbg13@gmail.com>
2022-07-21 11:14:16 +0900

    feat: 웹 훅을 통한 자동배포 구현

---
+++ backend/smody/src/main/java/com/woowacourse/smody/webhook/WebHookController.java
@@ -0,0 +1,30 @@
+package com.woowacourse.smody.webhook;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web-hook")
+public class WebHookController {
+
+    public static final String GITHUB_HOOK_ID = "369627662";
+    public static final String DEPLOY_BRANCH = "develop";
+
+    @PostMapping("/deploy")
+    public ResponseEntity<Void> deploy(@RequestHeader(value = "X-GitHub-Hook-ID") String gitHubHookId,
+                                       @RequestBody GitHubHookRequest gitHubHookRequest) {
+        if (gitHubHookId.equals(GITHUB_HOOK_ID) && gitHubHookRequest.parseBranch().equals(DEPLOY_BRANCH)) {
+            try {
+                Runtime.getRuntime().exec("/home/ubuntu/spring-deploy.sh");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
+}
