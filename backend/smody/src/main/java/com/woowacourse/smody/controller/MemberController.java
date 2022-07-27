065af11764af22d20a80e100596f0dc5bcaf1d4d
DOES <78652144+ldk980130@users.noreply.github.com>
2022-07-27 20:57:47 +0900

    style: (be) 로그 색상 추가 (#154)

--- backend/smody/src/main/java/com/woowacourse/smody/controller/MemberController.java
+++ backend/smody/src/main/java/com/woowacourse/smody/controller/MemberController.java
@@ -1,11 +1,5 @@
 package com.woowacourse.smody.controller;
 
-import com.woowacourse.smody.auth.LoginMember;
-import com.woowacourse.smody.dto.MemberResponse;
-import com.woowacourse.smody.dto.MemberUpdateRequest;
-import com.woowacourse.smody.dto.TokenPayload;
-import com.woowacourse.smody.service.MemberService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +7,14 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.woowacourse.smody.auth.LoginMember;
+import com.woowacourse.smody.dto.MemberResponse;
+import com.woowacourse.smody.dto.MemberUpdateRequest;
+import com.woowacourse.smody.dto.TokenPayload;
+import com.woowacourse.smody.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/members")
