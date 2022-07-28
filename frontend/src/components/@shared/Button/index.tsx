cc4e9bc48bbd8ce6e1a3a041cce5ee1933f88b4e
2019015414 <2019015414@hanyang.ac.kr>
2022-07-28 20:12:52 +0900

    test: PR후 jenkins build 실패 시 merge 비활성화 되는 지 확인

--- frontend/src/components/@shared/Button/index.tsx
+++ frontend/src/components/@shared/Button/index.tsx
@@ -8,34 +8,34 @@ const SIZES = {
   large: { minWidth: '100%', height: '45px', fontSize: '15px' },
 };
 
-export const Button = styled.button<ButtonProps>`
-  ${({ theme, size, isActive = true }) => css`
-    height: ${SIZES[size].height};
-    min-width: ${SIZES[size].minWidth};
-    width: fit-content;
-    vertical-align: middle;
-    text-align: center;
-    padding: 0rem 1rem;
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
+// export const Button = styled.button<ButtonProps>`
+//   ${({ theme, size, isActive = true }) => css`
+//     height: ${SIZES[size].height};
+//     min-width: ${SIZES[size].minWidth};
+//     width: fit-content;
+//     vertical-align: middle;
+//     text-align: center;
+//     padding: 0rem 1rem;
+//     background-color: transparent;
+//     border: none;
+//     cursor: pointer;
 
-    border-radius: 7px;
-    ${isActive
-      ? css`
-          background-color: ${theme.primary};
-          color: ${theme.onPrimary};
-        `
-      : css`
-          background-color: ${theme.secondary};
-          color: ${theme.onSecondary};
-        `}
+//     border-radius: 7px;
+//     ${isActive
+//       ? css`
+//           background-color: ${theme.primary};
+//           color: ${theme.onPrimary};
+//         `
+//       : css`
+//           background-color: ${theme.secondary};
+//           color: ${theme.onSecondary};
+//         `}
 
-    font-size: ${SIZES[size].fontSize};
+//     font-size: ${SIZES[size].fontSize};
 
-    &:disabled {
-      background-color: ${theme.disabled};
-      cursor: default;
-    }
-  `}
-`;
+//     &:disabled {
+//       background-color: ${theme.disabled};
+//       cursor: default;
+//     }
+//   `}
+// `;
