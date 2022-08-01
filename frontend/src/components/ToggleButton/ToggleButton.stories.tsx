52f3581072cdf2714864b42fed1e70774054d88f
Marco <bigdatajang@gmail.com>
2022-08-01 20:49:26 +0900

    fix: 스토리북 토글버튼 작동 문제 해결
    Co-authored-by: Woo seung <woose28@users.noreply.github.com>
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- frontend/src/components/ToggleButton/ToggleButton.stories.tsx
+++ frontend/src/components/ToggleButton/ToggleButton.stories.tsx
@@ -1,21 +1,31 @@
 import { ToggleButtonProps } from './type';
 
+import { useArgs } from '@storybook/client-api';
+
 import { ToggleButton } from 'components/ToggleButton';
 
 export default {
   title: 'components/ToggleButton',
   component: ToggleButton,
+  argTypes: {
+    handleChange: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 };
 
-export const DefaultToggleButton = (args: ToggleButtonProps) => (
-  <ToggleButton {...args}>🎁</ToggleButton>
-);
+export const DefaultToggleButton = (args: Pick<ToggleButtonProps, 'checked'>) => {
+  const [_, updateArgs] = useArgs();
 
-let checkedValue = false;
+  const handleChange = () => {
+    updateArgs({ checked: !args.checked });
+  };
+
+  return <ToggleButton {...args} handleChange={handleChange} />;
+};
+
 DefaultToggleButton.args = {
-  checked: checkedValue,
-  handleChange: () => {
-    checkedValue = !checkedValue;
-    console.log(checkedValue);
-  },
+  checked: true,
 };
