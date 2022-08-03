cc13a64519bed8e2f701d75291f5730fc42f592b
Marco <bigdatajang@gmail.com>
2022-08-04 03:27:52 +0900

    test: [useGetMyCyclesStat] query 응답에 따라 나의 챌린지 통계가 렌더링되는지 확인한다.

--- frontend/src/__test__/searchPage.test.tsx
+++ frontend/src/__test__/searchPage.test.tsx
@@ -5,9 +5,11 @@ import { screen, waitFor } from '@testing-library/react';
 import { SearchPage } from 'pages';
 
 describe('검색 페이지 테스트', () => {
-  test('[useGetAllChallenges] query 응답에 따라 모든 챌린지가 렌더링되는지 확인한다.', async () => {
+  beforeEach(() => {
     renderWithProviders(<SearchPage />);
+  });
 
+  test('[useGetAllChallenges] query 응답에 따라 모든 챌린지가 렌더링되는지 확인한다.', async () => {
     const challengeName = await waitFor(
       () => screen.getAllByLabelText('challenge-name')[0],
     );
