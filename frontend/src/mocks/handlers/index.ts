410529301bb20d68c413071c6696d5441641ff91
wonsss <bigdatajang@gmail.com>
2022-08-09 23:06:26 +0900

    chore: post 메서드 페이로드 수정

--- frontend/src/mocks/handlers/index.ts
+++ frontend/src/mocks/handlers/index.ts
@@ -2,4 +2,4 @@ import { auth } from 'mocks/handlers/auth';
 import { challenge } from 'mocks/handlers/challenge';
 import { push } from 'mocks/handlers/push';
 
-export const handlers = [...challenge, ...auth];
+export const handlers = [...challenge, ...auth, ...push];
