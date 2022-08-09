18da906139defb7b98ce765837aa7d233a9f5522
bcc0830 <bcc0830@bithub.com>
2022-08-09 13:40:29 +0900

    feat : Challenge 엔티티에 description, emoji 칼럼 추가

---
+++ backend/smody/src/main/java/com/woowacourse/smody/dto/ChallengesResponse.java
@@ -0,0 +1,34 @@
+package com.woowacourse.smody.dto;
+
+import com.woowacourse.smody.domain.Challenge;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ChallengesResponse {
+
+    private Long challengeId;
+    private String challengeName;
+    private Integer challengerCount;
+    private Boolean isInProgress;
+    private String emoji;
+
+    public ChallengesResponse(Challenge challenge, Integer challengerCount, Boolean isInProgress) {
+        this.challengeId = challenge.getId();
+        this.challengeName = challenge.getName();
+        this.challengerCount = challengerCount;
+        this.isInProgress = isInProgress;
+        this.emoji = challenge.getEmoji();
+    }
+
+    public ChallengesResponse(Challenge challenge, Integer challengerCount) {
+        this(challenge, challengerCount, false);
+    }
+
+    public ChallengesResponse changeInProgress(Boolean inProgress) {
+        return new ChallengesResponse(challengeId, challengeName, challengerCount, inProgress, emoji);
+    }
+}
