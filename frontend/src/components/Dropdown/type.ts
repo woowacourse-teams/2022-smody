a1998ca9ec9bca0f91ae65c54e31d66d211bcea7
Marco <bigdatajang@gmail.com>
2022-08-10 21:57:52 +0900

    refactor: Dropdown 컴포넌트의 재사용성을 위해 prop 리팩토링

--- frontend/src/components/Dropdown/type.ts
+++ frontend/src/components/Dropdown/type.ts
@@ -1,11 +1,6 @@
 import { ReactNode } from 'react';
 
-interface Item {
-  text: string;
-  linkTo: string;
-}
 export interface DropdownProps {
   button: ReactNode;
   children: ReactNode;
-  itemList: Item[];
 }
