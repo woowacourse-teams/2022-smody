1b02ce9cb7c080a61d751bc26702cff974b22010
woose28 <dldntmdsla@naver.com>
2022-08-10 16:11:44 +0900

    feat: FeedList의 FeedItem 사이 구분선 구현

--- frontend/src/pages/FeedPage/index.tsx
+++ frontend/src/pages/FeedPage/index.tsx
@@ -1,3 +1,5 @@
+import styled, { css } from 'styled-components';
+
 import { useFeedPage } from 'pages/FeedPage/useFeedPage';
 
 import { FlexBox, FeedItem, InfiniteScroll, LoadingSpinner } from 'components';
@@ -16,7 +18,7 @@ const FeedPage = () => {
       isFetching={isFetching}
       loader={<LoadingSpinner />}
     >
-      <FlexBox as="ul" flexDirection="column" alignItems="center">
+      <FeedList as="ul" flexDirection="column" alignItems="center">
         {feedInfiniteData?.pages.map((page) =>
           page?.data.map((feedInfo) => (
             <li key={feedInfo.cycleDetailId}>
@@ -24,9 +26,25 @@ const FeedPage = () => {
             </li>
           )),
         )}
-      </FlexBox>
+      </FeedList>
     </InfiniteScroll>
   );
 };
 
 export default FeedPage;
+
+const FeedList = styled(FlexBox)`
+  ${({ theme }) => css`
+    & li::after {
+      content: '';
+      display: block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.secondary};
+    }
+
+    & li:last-child::after {
+      display: none;
+    }
+  `}
+`;
