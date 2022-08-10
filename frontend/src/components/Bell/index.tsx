64b627b5fb3c76e34f3c0900ceeb2dc6e4d63768
wonsss <bigdatajang@gmail.com>
2022-08-11 01:13:40 +0900

    feat: 구독하면 벨 스타일 채워지기

--- frontend/src/components/Bell/index.tsx
+++ frontend/src/components/Bell/index.tsx
@@ -1,17 +1,18 @@
 import { BellProps } from './type';
-import BellIcon from 'assets/bell_icon.svg';
+import BellEmptyIcon from 'assets/bell_empty_icon.svg';
+import BellFilledIcon from 'assets/bell_filled_icon.svg';
 import styled, { css } from 'styled-components';
 
 import useThemeContext from 'hooks/useThemeContext';
 
 import { AvailablePickedColor } from 'styles/type';
 
-export const Bell = ({ count }: BellProps) => {
+export const Bell = ({ count, isSubscribed }: BellProps) => {
   const themeContext = useThemeContext();
 
   return (
     <IconWrapper fill={themeContext.primary}>
-      <BellIcon />
+      {isSubscribed ? <BellFilledIcon /> : <BellEmptyIcon />}
       {!!count && <BellCount>{count}</BellCount>}
     </IconWrapper>
   );
@@ -23,25 +24,22 @@ const IconWrapper = styled.div`
 
     & svg path {
       fill: ${fill};
-      stroke: none;
     }
   `}
 `;
 
 const BellCount = styled.em`
-  ${({ theme }) => css`
-    position: absolute;
-    top: 1px;
-    right: -1px;
-    width: 0.9rem;
-    height: 0.9rem;
-    border-radius: 50%;
-    background-color: ${theme.error};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 0.7rem;
-    padding-top: 1px;
-    color: #fff;
-  `}
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  position: absolute;
+  top: -2px;
+  right: -7px;
+  width: 1.3rem;
+  height: 1.3rem;
+  font-size: 0.9rem;
+  padding-top: 2px;
+  border-radius: 50%;
+  background-color: red;
+  color: white;
 `;
--- frontend/src/components/Bell/type.ts
+++ frontend/src/components/Bell/type.ts
@@ -1,3 +1,4 @@
 export interface BellProps {
   count: number;
+  isSubscribed: boolean;
 }
