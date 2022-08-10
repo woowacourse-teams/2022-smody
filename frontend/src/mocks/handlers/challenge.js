91a7641874f887869f5497c3b1e558243867e4f6
2019015414 <2019015414@hanyang.ac.kr>
2022-08-11 03:57:05 +0900

    feat: 챌린지 생성 API mocking

--- frontend/src/mocks/handlers/challenge.js
+++ frontend/src/mocks/handlers/challenge.js
@@ -148,4 +148,11 @@ export const challenge = [
     // return res(ctx.status(200), ctx.json([]));
     return res(ctx.status(200), ctx.json(challengers));
   }),
+
+  //10. 챌린지 생성(POST)
+  rest.post(`${BASE_URL}/challenges`, (req, res, ctx) => {
+    challengeData.push(req.body);
+    console.log(req.body);
+    return res(ctx.delay(1000), ctx.json(201));
+  }),
 ];
