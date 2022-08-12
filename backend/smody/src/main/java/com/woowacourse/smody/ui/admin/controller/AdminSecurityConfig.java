5dd15b94df6e47317f8cc7108f5759a8e452f016
bcc0830 <50986686+bcc0830@users.noreply.github.com>
2022-08-12 14:55:20 +0900

    fix : 시큐리티 해제 url 추가 (#263)
    Co-authored-by: bcc0830 <bcc0830@bithub.com>

--- backend/smody/src/main/java/com/woowacourse/smody/ui/admin/controller/AdminSecurityConfig.java
+++ backend/smody/src/main/java/com/woowacourse/smody/ui/admin/controller/AdminSecurityConfig.java
@@ -28,7 +28,7 @@ public class AdminSecurityConfig extends VaadinWebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) {
         web.ignoring().antMatchers(
                 "/challenges/**", "/cycles/**", "/members/**", "/oauth/**", "/docs/**", "/images/**",
-                "/feeds/**", "/web-push/**"
+                "/feeds/**", "/web-push/**", "/profile/**"
         );
     }
 
