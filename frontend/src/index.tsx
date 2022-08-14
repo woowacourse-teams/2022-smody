5fb50a5b3a9890d0b9864bfbd84c3d4ed68413f8
Marco <bigdatajang@gmail.com>
2022-08-14 22:36:26 +0900

    chore: production, local, development 주석 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,12 @@ import registerPushServiceWorker from 'push/registerPushServiceWorker';
 import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 
-if (process.env.NODE_ENV === 'production') {
+// production or local
+if (process.env.NODE_ENV === 'production' || process.env.IS_LOCAL) {
   registerPushServiceWorker();
 }
 
+// development
 if (process.env.NODE_ENV === 'development' && !process.env.IS_LOCAL) {
   const { mockServiceWorker } = require('./mocks/browser');
   mockServiceWorker.start();
