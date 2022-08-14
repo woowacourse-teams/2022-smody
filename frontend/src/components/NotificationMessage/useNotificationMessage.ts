77c49712e13c24f155c36569c4bb2d97a732b776
Marco <bigdatajang@gmail.com>
2022-08-14 22:08:04 +0900

    chore: 주석 삭제

--- frontend/src/components/NotificationMessage/useNotificationMessage.ts
+++ frontend/src/components/NotificationMessage/useNotificationMessage.ts
@@ -30,9 +30,6 @@ export const useNotificationMessage = () => {
     if (pushCase === 'challenge') {
       navigate(`/cycle/detail/${pathId}`);
     }
-    // TODO : 네비게이트 시킬 라우터 패스, 백엔드로부터 받기
-    // 인증 마감 임박 알릴 경우 -> cycleId 받기 '/cycle/detail/:cycleId',
-    // 댓글 달릴 경우 -> cycleDetailId 받기 '/feed/detail/:cycleDetailId',
   };
 
   return { handleClickNotification };
