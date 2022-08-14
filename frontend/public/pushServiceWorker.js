89437b44b098c7384008121b96288a8f9385c1b6
Marco <bigdatajang@gmail.com>
2022-08-14 21:00:40 +0900

    feat: 알림의 icon 및 link 등 추가 위해 serviceWorker 수정

--- frontend/public/pushServiceWorker.js
+++ frontend/public/pushServiceWorker.js
@@ -18,16 +18,36 @@ self.addEventListener('push', (event) => {
   const data = event.data.json();
   console.log('Service Worker - push:', data);
 
-  const title = 'Smody';
+  const title = 'Three more days, 스모디';
+  const type = data.type;
+  const pathId = data.pathId;
+  let path;
+  if (type === 'subscription') {
+    path = `/profile`;
+  }
+  if (type === 'comment') {
+    path = `/feed/detail/${pathId}`;
+  }
+  if (type === 'challenge') {
+    path = `/cycle/detail/${pathId}`;
+  }
+
   const options = {
     body: data.message,
+    icon: './image/favicon-32x32.png',
+    badge: './image/favicon-16x16.png',
+    vibrate: [
+      500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40, 500,
+    ],
+    data: { path },
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
 });
 
 self.addEventListener('notificationclick', (event) => {
-  console.log('Service Worker - Notification clicked!');
   event.notification.close();
-  event.waitUntil(self.clients.openWindow('https://www.smody.com'));
+  const fullPath = self.location.origin + event.notification.data.path;
+
+  event.waitUntil(self.clients.openWindow(fullPath));
 });
