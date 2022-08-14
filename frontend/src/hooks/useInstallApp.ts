fcbf1d6ab06dbd0ddbfacb27df36ac876b087c4a
Marco <bigdatajang@gmail.com>
2022-08-15 02:20:19 +0900

    feat: 앱 설치 불가 메시지 단순화

--- frontend/src/hooks/useInstallApp.ts
+++ frontend/src/hooks/useInstallApp.ts
@@ -11,18 +11,9 @@ const useInstallApp = () => {
   const renderSnackBar = useSnackBar();
 
   const installApp = () => {
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-      renderSnackBar({
-        message: '이미 앱이 설치되어 있습니다. 앱으로 접속해주세요.',
-        status: 'ERROR',
-      });
-      return;
-    }
-
     if (!deferredPrompt) {
       renderSnackBar({
-        message:
-          '앱 설치가 불가한 OS 또는 브라우저입니다. Android 및 Chrome을 권장합니다.',
+        message: '이미 앱이 설치되어 있거나 앱을 설치할 수 없는 환경입니다.',
         status: 'ERROR',
       });
       return;
