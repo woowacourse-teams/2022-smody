7a5bb144ee517ae162f5a29b2b0178abbc315a66
2019015414 <2019015414@hanyang.ac.kr>
2022-08-16 22:09:39 +0900

    feat: useMatchPath에서 exceptRouter 로직 추가

--- frontend/src/hooks/useMatchPath.ts
+++ frontend/src/hooks/useMatchPath.ts
@@ -2,8 +2,20 @@ import { useLocation } from 'react-router-dom';
 
 const useMatchPath = <T>(matchedData: T, unMatchedData: T) => {
   const { pathname } = useLocation();
-  const getPathMatchResult = (routes: string[]) => {
-    let matchResult = unMatchedData;
+  const getPathMatchResult = (routes: string[], exceptRoutes?: string[]) => {
+    let matchResult = matchedData;
+    if (exceptRoutes) {
+      exceptRoutes.forEach((route) => {
+        if (pathname.includes(route)) {
+          matchResult = unMatchedData;
+          return;
+        }
+      });
+      if (matchResult === unMatchedData) {
+        return matchResult;
+      }
+    }
+    matchResult = unMatchedData;
     routes.forEach((route) => {
       if (pathname.includes(route)) {
         matchResult = matchedData;
