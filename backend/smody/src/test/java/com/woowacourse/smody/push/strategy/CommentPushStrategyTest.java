94f7a04e1fa7a3172558e5476441b3fff0a18a28
DOES <ldk980130@gmail.com>
2022-08-17 11:59:10 +0900

    feat: 자신의 피드에 댓글을 달면 알림을 받지 않는 기능 구현

--- backend/smody/src/test/java/com/woowacourse/smody/push/strategy/CommentPushStrategyTest.java
+++ backend/smody/src/test/java/com/woowacourse/smody/push/strategy/CommentPushStrategyTest.java
@@ -7,7 +7,9 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,14 +31,19 @@ class CommentPushStrategyTest extends IntegrationTest {
 	@Autowired
 	private PushNotificationRepository pushNotificationRepository;
 
+	private  CycleDetail cycleDetail;
+
+	@BeforeEach
+	void init() {
+		LocalDateTime now = LocalDateTime.now();
+		Cycle cycle = fixture.사이클_생성_FIRST(조조그린_ID, 미라클_모닝_ID, now);
+		cycleDetail = cycle.getCycleDetails().get(0);
+	}
+
 	@DisplayName("댓글을 남기면 피드 작성자에게 발송 상태의 알림이 저장 된다.")
 	@Test
 	void push() {
 		// given
-		LocalDateTime now = LocalDateTime.now();
-		Cycle cycle = fixture.사이클_생성_FIRST(조조그린_ID, 미라클_모닝_ID, now);
-		CycleDetail cycleDetail = cycle.getCycleDetails().get(0);
-
 		Comment comment = fixture.댓글_등록(cycleDetail, 더즈_ID, "댓글입니다.");
 
 		// when
@@ -56,17 +63,12 @@ class CommentPushStrategyTest extends IntegrationTest {
 		);
 	}
 
-	@DisplayName("알림을 구독했으면 알림 저장에 전송까지 실행된다.")
+	@DisplayName("알림을 구독했으면 댓글을 달았을 때 알림 저장에 전송까지 실행된다.")
 	@Test
 	void push_existSubscription() {
 		// given
-		LocalDateTime now = LocalDateTime.now();
-		Cycle cycle = fixture.사이클_생성_FIRST(조조그린_ID, 미라클_모닝_ID, now);
-		CycleDetail cycleDetail = cycle.getCycleDetails().get(0);
-
-		Comment comment = fixture.댓글_등록(cycleDetail, 더즈_ID, "댓글입니다.");
-
 		fixture.알림_구독(조조그린_ID, "endpoint");
+		Comment comment = fixture.댓글_등록(cycleDetail, 더즈_ID, "댓글입니다.");
 
 		// when
 		pushStrategy.push(comment);
@@ -81,6 +83,25 @@ class CommentPushStrategyTest extends IntegrationTest {
 			() -> assertThat(pushNotification.getPushCase()).isEqualTo(PushCase.COMMENT),
 			() -> assertThat(pushNotification.getPathId()).isEqualTo(cycleDetail.getId()),
 			() -> verify(webPushService)
+				.sendNotification(any(), any())
+		);
+	}
+
+	@DisplayName("자신의 게시글에 댓글을 달면 알림이 전송, 저장되지 않는다.")
+	@Test
+	void push_commentMyFeed() {
+		// given
+		Comment comment = fixture.댓글_등록(cycleDetail, 조조그린_ID, "댓글입니다.");
+		fixture.알림_구독(조조그린_ID, "endpoint");
+
+		// when
+		pushStrategy.push(comment);
+
+		// then
+		List<PushNotification> results = pushNotificationRepository.findByPushStatus(PushStatus.COMPLETE);
+		assertAll(
+			() -> assertThat(results).isEmpty(),
+			() -> verify(webPushService, never())
 				.sendNotification(any(), any())
 		);
 	}
