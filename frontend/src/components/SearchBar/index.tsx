3c35ce271676b0535db37fc4f60c52811324f27d
ronci <jyj9647@naver.com>
2022-08-18 12:10:24 +0900

    feat: (fe) 검색 바 상단에 고정시키도록 구현

--- frontend/src/components/SearchBar/index.tsx
+++ frontend/src/components/SearchBar/index.tsx
@@ -2,16 +2,46 @@ import { SearchBarProps } from './type';
 import Search from 'assets/search.svg';
 import styled, { css } from 'styled-components';
 
+const horizontalPadding = { pc: '10rem', tablet: '7rem', mobile: '1.25rem' };
+
 export const SearchBar = ({ search, handleSubmitSearch }: SearchBarProps) => {
   return (
-    <InputWrapper onSubmit={handleSubmitSearch}>
-      <InputElement type="text" placeholder="챌린지 검색" {...search} />
-      <button type="submit">
-        <Search />
-      </button>
-    </InputWrapper>
+    <Background>
+      <InputWrapper onSubmit={handleSubmitSearch}>
+        <InputElement type="text" placeholder="챌린지 검색" {...search} />
+        <button type="submit">
+          <Search />
+        </button>
+      </InputWrapper>
+    </Background>
   );
 };
+
+const Background = styled.div`
+  ${({ theme }) => css`
+    position: fixed;
+    top: 60px;
+    background-color: ${theme.background};
+    padding: 5px 0;
+
+    @media all and (min-width: 1024px) {
+      left: ${horizontalPadding.pc};
+      right: ${horizontalPadding.pc};
+    }
+
+    /* 테블릿 가로, 테블릿 세로 (해상도 768px ~ 1023px)*/
+    @media all and (min-width: 768px) and (max-width: 1023px) {
+      left: ${horizontalPadding.tablet};
+      right: ${horizontalPadding.tablet};
+    }
+
+    /* 모바일 가로, 모바일 세로 (해상도 480px ~ 767px)*/
+    @media all and (max-width: 767px) {
+      left: ${horizontalPadding.mobile};
+      right: ${horizontalPadding.mobile};
+    }
+  `}
+`;
 
 const InputWrapper = styled.form`
   ${({ theme }) => css`
@@ -21,7 +51,6 @@ const InputWrapper = styled.form`
 
     border-radius: 20px;
     padding: 0.5rem;
-    margin-bottom: 1rem;
 
     & svg path {
       fill: ${theme.primary};
