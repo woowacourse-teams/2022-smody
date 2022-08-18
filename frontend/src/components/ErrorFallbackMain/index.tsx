312493e657dfab5c652d8bd363a099b569a66727
woose28 <dldntmdsla@naver.com>
2022-08-18 15:03:52 +0900

    feat: 로그아웃 처리는 되는 에러 범위 확대
    - '2003: 인가되지 않은 회원입니다.', '4001: 존재하지 않는 회원입니다.' 에러 발생 시 로그아웃 처리

--- frontend/src/components/ErrorFallbackMain/index.tsx
+++ frontend/src/components/ErrorFallbackMain/index.tsx
@@ -26,7 +26,7 @@ export const ErrorFallbackMain = ({
     });
 
     // 로그아웃 처리
-    if (errorCode === 2002) {
+    if (errorCode === 2002 || errorCode === 2003 || errorCode === 4001) {
       authApiClient.deleteAuth();
       setIsLogin(false);
     }
