6e9081f85ebce619ce904ba35daca417012cc06c
Woo seung <52148907+woose28@users.noreply.github.com>
2022-09-15 10:34:23 +0900

    fix: (fe) 피드 상세 페이지 챌린지 이름 관련 누락된 코드 반영 (#393)
    * fix: 피드 상세 보기 페이지에서 챌린지 이름이 안 눌리는 문제 해결
    
    - FeedItem 컴포넌트의 ChallengeName css pointer-events 속성에 auto 값 추가
    
    * feat: 피드 상세 보기 페이지에서는 전체 챌린지 이름을 보여주도록 구현
    
    - isShowBriefChallengeName props에 따라 챌린지 이름을 요약하도록 구현
    
    * refactor: challengeName 렌더링 로직 커스텀 훅으로 이동
    
    * fix: 댓글 작성 버튼의 불필요한 padding 제거

--- frontend/src/components/FeedItem/index.tsx
+++ frontend/src/components/FeedItem/index.tsx
@@ -45,27 +45,23 @@ export const FeedItem = ({
       onClick={handleClickFeed}
       isClickable={isClickable}
     >
-      <FlexBox alignItems="center">
+      <FlexBox alignItems="center" flexWrap="wrap">
         <ProfileImg src={picture} alt={`${nickname}님의 프로필 사진`} loading="lazy" />
-        <FlexBox alignItems="center">
-          <Nickname size={20} color={themeContext.onBackground}>
-            {nickname}
-          </Nickname>
-          <OfText size={16} color={themeContext.onBackground}>
-            의&nbsp;
-          </OfText>
-          <UnderLineText
-            fontSize={20}
-            fontColor={themeContext.onBackground}
-            underLineColor={themeContext.primary}
-            fontWeight="bold"
-            onClick={handleClickChallengeName}
-          >
-            {challengeName.length > 9
-              ? `${challengeName.substring(0, 9)}...`
-              : challengeName}
-          </UnderLineText>
-        </FlexBox>
+        <Nickname size={20} color={themeContext.onBackground}>
+          {nickname}
+        </Nickname>
+        <OfText size={16} color={themeContext.onBackground}>
+          의&nbsp;
+        </OfText>
+        <ChallengeName
+          fontSize={20}
+          fontColor={themeContext.onBackground}
+          underLineColor={themeContext.primary}
+          fontWeight="bold"
+          onClick={handleClickChallengeName}
+        >
+          {renderedChallengeName}
+        </ChallengeName>
       </FlexBox>
       <ProgressImg
         src={progressImage}
