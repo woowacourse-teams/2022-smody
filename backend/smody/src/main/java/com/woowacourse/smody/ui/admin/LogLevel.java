f25046aad7822f7227f6b8319ed7f466d6965c90
DOES <78652144+ldk980130@users.noreply.github.com>
2022-09-21 20:11:51 +0900

    fix: 로그 색깔 제외 (#416)

--- backend/smody/src/main/java/com/woowacourse/smody/ui/admin/LogLevel.java
+++ backend/smody/src/main/java/com/woowacourse/smody/ui/admin/LogLevel.java
@@ -1,6 +1,5 @@
 package com.woowacourse.smody.ui.admin;
 
-import com.vaadin.flow.component.html.Span;
 import lombok.Getter;
 
 @Getter
@@ -18,16 +17,5 @@ public enum LogLevel {
     LogLevel(String text, String color) {
         this.text = text;
         this.color = color;
-    }
-
-    public void setColor(String line, Span span) {
-        String[] split = line.split(this.text);
-        span.add(split[0]);
-
-        Span info = new Span(this.text);
-        info.getStyle().set("color", this.color);
-        span.add(info);
-
-        span.add(split[1]);
     }
 }
