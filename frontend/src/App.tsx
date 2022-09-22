49a11492e9b148b2a6613e4557892749d2bcec64
Marco <59413128+wonsss@users.noreply.github.com>
2022-09-22 22:15:17 +0900

    fix: (fe) refetch 하면서 indexedDB 중복저장하던 문제 해결 (#446)
    Co-authored-by: ronci <ronci@users.noreply.github.com>
    Co-authored-by: Woo seung <woose28@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ const queryClient = new QueryClient({
       retry: 0,
       suspense: true,
       useErrorBoundary: true,
+      refetchOnWindowFocus: false,
     },
     mutations: {
       useErrorBoundary: true,
