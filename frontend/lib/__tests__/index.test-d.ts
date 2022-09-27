afa05fa3d7b6ee497750706c596286b1b0fe5a38
Marco <bigdatajang@gmail.com>
2022-09-27 21:38:34 +0900

    refactor: MappedKeyToUnion 테스트 리팩터링

--- frontend/lib/__tests__/index.test-d.ts
+++ frontend/lib/__tests__/index.test-d.ts
@@ -1,12 +1,9 @@
 import { MappedKeyToUnion } from '../dist';
-import { Expect, Equal } from './test-util';
+import { Equal } from './test-util';
 import { expectType } from 'tsd';
 
-test('foo test', () => {
-  const COLOR_1 = { LIGHT_PURPLE: 1, bb: '2' } as const;
-  const COLOR_2 = { aaa: 1, bb: '2' } as const;
-
-  expectType<
-    Expect<Equal<MappedKeyToUnion<typeof COLOR_1>, MappedKeyToUnion<typeof COLOR_2>>>
-  >(true);
+test('MappedKeyToUnion 타입을 테스트한다. `{ a: 1; b: "2" }` 객체에 대하여 MappedKeyToUnion을 통해 기대하는 타입인 `1 | "2"` 이라는 유니온 타입이 반환되는지 확인한다.', () => {
+  type MockData = { a: 1; b: '2' };
+  type ExpectedType = 1 | '2';
+  expectType<Equal<MappedKeyToUnion<MockData>, ExpectedType>>(true);
 });
