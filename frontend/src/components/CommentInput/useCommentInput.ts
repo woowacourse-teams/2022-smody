cc9be3edd134a4627f1fc3c87eb6c7948419df6d
Marco <bigdatajang@gmail.com>
2022-09-29 22:35:08 +0900

    refactor: 멘션 @ 작성 시 API 로직 연결 부분 리팩토링
    Co-authored-by: Woo seung <woose28@users.noreply.github.com>

--- frontend/src/components/CommentInput/useCommentInput.ts
+++ frontend/src/components/CommentInput/useCommentInput.ts
@@ -16,6 +16,7 @@ import { CLIENT_PATH } from 'constants/path';
 
 const DEFAULT_INPUT_HEIGHT = '1.5rem';
 const INITIAL_CONTENT = '';
+const ABSENCE_SYMBOL_POSITION = -1;
 
 const useCommentInput = ({
   selectedCommentId,
@@ -25,55 +26,46 @@ const useCommentInput = ({
   // ------- 멘션 알림 기능 ------------------------
   const flagCheck = useRef(false);
   const [filterValue, setFilterValue] = useState('');
-  const lastMentionSymbolPositionRef = useRef(-1);
+  const lastMentionSymbolPositionRef = useRef(ABSENCE_SYMBOL_POSITION);
 
   useEffect(() => {
     if (!flagCheck.current) {
       flagCheck.current = true;
       return;
     }
-
-    console.log('useEffect에서 filterValue', filterValue);
     refetch();
   }, [filterValue]);
 
   const inputChangeHandler: MutationCallback = (mutations) => {
-    console.log(
-      '필터밸류',
-      filterValue,
-      'lastSymbolPostion: ',
-      lastMentionSymbolPositionRef.current,
-    );
+    const hasSymbolPosition =
+      lastMentionSymbolPositionRef.current !== ABSENCE_SYMBOL_POSITION;
+
+    const isCurrentCharacterWhiteSpace = (text: string) =>
+      text[getCursorPosition() - 1] === ' ';
+
+    const setNicknameAfterMentionSymbol = (text: string) => {
+      if (isCurrentCharacterWhiteSpace(text)) {
+        lastMentionSymbolPositionRef.current = ABSENCE_SYMBOL_POSITION;
+      } else {
+        setFilterValue(
+          text.slice(lastMentionSymbolPositionRef.current, getCursorPosition()),
+        );
+      }
+    };
+
     const commentInputElement = commentInputRef.current!;
     resizeHeight(commentInputElement);
     const { innerText } = commentInputElement;
 
-    // Symbol 위치 정보 저장 여부 판단.
-
-    // symbol의 위치 정보 없어?
-    if (lastMentionSymbolPositionRef.current === -1) {
-      // symbol의 위치를 찾아서 알려만 줘
-      detectMentionSymbol(innerText);
-      console.log('왔니1');
+    if (hasSymbolPosition) {
+      setNicknameAfterMentionSymbol(innerText);
     } else {
-      // symbol 위치 정보 있으면, 이제 일을 하자.
-      // 현재 입력 값이 공백이냐? 어 그럼 말이 달라지지.
-
-      if (innerText[getCursorPosition() - 1] === ' ') {
-        // symbol 위치 정보 제거 - 다시 symbol 위치 정보 없앨거야
-        lastMentionSymbolPositionRef.current = -1;
-        console.log('왔니2');
-      } else {
-        // 현재 입력 값이 뭐라도 있네. 그러면, 방금 @부터 지금 커서까지 filterValue를 만들어 줄게
-        setFilterValue(
-          innerText.slice(lastMentionSymbolPositionRef.current, getCursorPosition()),
-        );
-        console.log('왔니3');
-      }
+      detectMentionSymbol(innerText);
     }
 
     setContent(innerText.slice(0, MAX_TEXTAREA_LENGTH));
   };
+
   const commentInputRef = useMutationObserver<HTMLDivElement>(inputChangeHandler);
 
   const {
@@ -104,19 +96,10 @@ const useCommentInput = ({
 
     // 2. 그런데 호출하려는 @의 앞이 공백이 아니면 호출하지 않는다.
     if (cursorPosition !== 1 && text.length >= 2 && text[cursorPosition - 2] !== ' ') {
-      // symbol 위치 정보 제거
       return;
     }
 
-    // 멤버 조회 API 요청
-    // console.log(text, cursorPosition);
-    // setLastMentionSymbolPosition(cursorPosition);
-
-    // flagFoundMentionSymbol.current = true;
-    // setFilterValue();
-    // setLastMentionSymbolPosition(1000000);
     lastMentionSymbolPositionRef.current = cursorPosition;
-    console.log('setLastMentionSymbolPosition', lastMentionSymbolPositionRef.current);
   };
 
   // ---------- 댓글 작성 및 수정하여 db에 보내는 관련 로직 ----------------
