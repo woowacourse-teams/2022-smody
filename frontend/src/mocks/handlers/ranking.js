133a28779ff68803163e31a5bb03e2ddacf46a0b
2019015414 <2019015414@hanyang.ac.kr>
2022-09-30 17:51:10 +0900

    fix: mocking method 수정

--- frontend/src/mocks/handlers/ranking.js
+++ frontend/src/mocks/handlers/ranking.js
@@ -4,7 +4,7 @@ import { rest } from 'msw';
 
 export const ranking = [
   //전체 랭킹 기간 조회
-  rest.post(`${BASE_URL}/ranking-periods?sort=startDate desc`, (req, res, ctx) => {
+  rest.get(`${BASE_URL}/ranking-periods`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(rankingPeriodsData));
   }),
 ];
