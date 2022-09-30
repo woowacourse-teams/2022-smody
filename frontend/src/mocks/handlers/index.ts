acabe77a2da9328a9da45db0352041d4f21aea34
2019015414 <2019015414@hanyang.ac.kr>
2022-09-30 17:51:10 +0900

    fix: mocking method 수정

--- frontend/src/mocks/handlers/index.ts
+++ frontend/src/mocks/handlers/index.ts
@@ -1,7 +1,7 @@
-import { ranking } from 'mocks/handlers//ranking';
 import { auth } from 'mocks/handlers/auth';
 import { challenge } from 'mocks/handlers/challenge';
 import { feed } from 'mocks/handlers/feed';
 import { push } from 'mocks/handlers/push';
+import { ranking } from 'mocks/handlers/ranking';
 
 export const handlers = [...challenge, ...auth, ...push, ...feed, ...ranking];
