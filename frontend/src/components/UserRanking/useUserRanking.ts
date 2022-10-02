6fdebee0dd9df696696ecbde484e7022f645afac
2019015414 <2019015414@hanyang.ac.kr>
2022-10-03 01:21:25 +0900

    feat: UserRanking skeleton 구현

--- frontend/src/components/UserRanking/useUserRanking.ts
+++ frontend/src/components/UserRanking/useUserRanking.ts
@@ -1,10 +1,39 @@
 import { UseUserRankingProps } from './type';
 import { useGetMyRanking } from 'apis';
+import { useEffect } from 'react';
+
+import { INIT_RANKING_PERIOD_ID } from 'constants/domain';
+
+const MockData = {
+  data: {
+    ranking: -1,
+    memberId: -1,
+    nickname: '',
+    introduction: '',
+    picture: '',
+    point: -1,
+  },
+};
 
 const useUserRanking = ({ rankingPeriodId }: UseUserRankingProps) => {
-  const { data: myRankingData } = useGetMyRanking({ rankingPeriodId });
+  const {
+    isSuccess: isSuccessMyRanking,
+    data: myRankingData,
+    refetch: refetchMyRanking,
+  } = useGetMyRanking({ rankingPeriodId }, { enabled: false, suspense: false });
+  let needSkeleton = !isSuccessMyRanking || rankingPeriodId === INIT_RANKING_PERIOD_ID;
 
-  return { myRankingData };
+  useEffect(() => {
+    if (rankingPeriodId !== INIT_RANKING_PERIOD_ID) {
+      refetchMyRanking();
+      needSkeleton = !isSuccessMyRanking || rankingPeriodId === INIT_RANKING_PERIOD_ID;
+    }
+  }, [rankingPeriodId]);
+
+  if (rankingPeriodId !== INIT_RANKING_PERIOD_ID) {
+    return { myRankingData, needSkeleton };
+  }
+  return { myRankingData: MockData, needSkeleton: true };
 };
 
 export default useUserRanking;
