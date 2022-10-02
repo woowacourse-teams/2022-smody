a92ca1afb794626b496523eb2f4ba60a806d6dad
2019015414 <2019015414@hanyang.ac.kr>
2022-10-03 01:20:32 +0900

    feat: RankingItem에 lazy loading 추가

--- frontend/src/components/RankingItem/index.tsx
+++ frontend/src/components/RankingItem/index.tsx
@@ -24,6 +24,7 @@ export const RankingItem = ({
         aria-label="프로필 이미지"
         src={picture}
         alt={`${nickname} 프로필 사진`}
+        loading="lazy"
       />
       <UserInfoWrapper flexDirection="column" gap="0.4rem">
         <Text
