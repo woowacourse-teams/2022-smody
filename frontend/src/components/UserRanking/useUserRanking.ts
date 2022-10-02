ba107405252107d420d0ed24da5814b0593428da
2019015414 <2019015414@hanyang.ac.kr>
2022-10-03 02:05:34 +0900

    feat: 랭킹에 참가하지 않았을 때 UI 구현

--- frontend/src/components/UserRanking/useUserRanking.ts
+++ frontend/src/components/UserRanking/useUserRanking.ts
@@ -1,6 +1,6 @@
 import { UseUserRankingProps } from './type';
 import { useGetMyRanking } from 'apis';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { INIT_RANKING_PERIOD_ID } from 'constants/domain';
 
@@ -16,24 +16,44 @@ const MockData = {
 };
 
 const useUserRanking = ({ rankingPeriodId }: UseUserRankingProps) => {
+  const [notFoundInRanking, setNotFoundInRanking] = useState(false);
   const {
     isSuccess: isSuccessMyRanking,
     data: myRankingData,
     refetch: refetchMyRanking,
-  } = useGetMyRanking({ rankingPeriodId }, { enabled: false, suspense: false });
+  } = useGetMyRanking(
+    { rankingPeriodId },
+    {
+      enabled: false,
+      suspense: false,
+      useErrorBoundary: (error) => error.response?.status !== 404,
+      onError: (error) => {
+        if (typeof error.response === 'undefined') {
+          return;
+        }
+
+        if (error.response.status === 404 && notFoundInRanking === false) {
+          setNotFoundInRanking(true);
+        }
+      },
+    },
+  );
   let needSkeleton = !isSuccessMyRanking || rankingPeriodId === INIT_RANKING_PERIOD_ID;
 
   useEffect(() => {
     if (rankingPeriodId !== INIT_RANKING_PERIOD_ID) {
+      if (notFoundInRanking) {
+        setNotFoundInRanking(false);
+      }
       refetchMyRanking();
       needSkeleton = !isSuccessMyRanking || rankingPeriodId === INIT_RANKING_PERIOD_ID;
     }
   }, [rankingPeriodId]);
 
   if (rankingPeriodId !== INIT_RANKING_PERIOD_ID) {
-    return { myRankingData, needSkeleton };
+    return { myRankingData, needSkeleton, notFoundInRanking };
   }
-  return { myRankingData: MockData, needSkeleton: true };
+  return { myRankingData: MockData, needSkeleton: true, notFoundInRanking: false };
 };
 
 export default useUserRanking;
