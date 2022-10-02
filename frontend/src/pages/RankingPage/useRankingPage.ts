05837d1a015b06d843bbb48a5a58964342db0449
2019015414 <2019015414@hanyang.ac.kr>
2022-10-03 01:21:02 +0900

    refactor: -1을 INIT_RANKING_PERIOD_ID로 상수화

--- frontend/src/pages/RankingPage/useRankingPage.ts
+++ frontend/src/pages/RankingPage/useRankingPage.ts
@@ -1,8 +1,11 @@
 import { HandleRankingPeriodIdFunc } from './type';
 import { useState } from 'react';
 
+import { INIT_RANKING_PERIOD_ID } from 'constants/domain';
+
 const useRankingPage = () => {
-  const [selectedRankingPeriodId, setSelectedRankingPeriodId] = useState(-1);
+  const [selectedRankingPeriodId, setSelectedRankingPeriodId] =
+    useState(INIT_RANKING_PERIOD_ID);
 
   const handleRankingPeriodId: HandleRankingPeriodIdFunc = ({ rankingPeriodId }) => {
     setSelectedRankingPeriodId(rankingPeriodId);
