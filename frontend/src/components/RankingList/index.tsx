93c3d4ed59caad0da346337f4d89b54b44f4c761
2019015414 <2019015414@hanyang.ac.kr>
2022-10-03 17:18:46 +0900

    feat: RankingList 데이터가 없을 때, 로딩 중일 때 UI 구현

---
+++ frontend/src/components/RankingList/index.tsx
@@ -0,0 +1,96 @@
+import useRankingList from './useRankingList';
+import styled, { css } from 'styled-components';
+
+import useThemeContext from 'hooks/useThemeContext';
+
+import { FlexBox, Text } from 'components';
+
+export const RankingList = () => {
+  const themeContext = useThemeContext();
+  const { allRankingData, needSkeleton, emptyRanking } = useRankingList();
+
+  if (emptyRanking) {
+    return (
+      <Wrapper flexDirection="column" gap="0.5rem">
+        <Text size={24} color={themeContext.onBackground} fontWeight="bold">
+          전체 랭킹
+        </Text>
+        <EmptyRankingList />
+      </Wrapper>
+    );
+  }
+
+  if (needSkeleton || typeof allRankingData?.data === 'undefined') {
+    return (
+      <Wrapper flexDirection="column" gap="0.5rem">
+        <Text size={24} color={themeContext.onBackground} fontWeight="bold">
+          전체 랭킹
+        </Text>
+        <AllRankingListSkeleton />
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper flexDirection="column" gap="0.5rem">
+      <Text size={24} color={themeContext.onBackground} fontWeight="bold">
+        전체 랭킹
+      </Text>
+      {/* <RankingItem {...myRankingData.data} /> */}
+    </Wrapper>
+  );
+};
+
+const AllRankingListSkeleton = () => {
+  const themeContext = useThemeContext();
+
+  return (
+    <NonDataWrapper
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap="1rem"
+    >
+      <Text size={20} color={themeContext.onSurface} fontWeight="bold">
+        전체 랭킹을 불러오는 중입니다...
+      </Text>
+      <Text size={16} color={themeContext.onSurface}>
+        잠시만 기다려주세요!
+      </Text>
+    </NonDataWrapper>
+  );
+};
+
+const EmptyRankingList = () => {
+  const themeContext = useThemeContext();
+
+  return (
+    <NonDataWrapper
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap="1rem"
+    >
+      <Text size={20} color={themeContext.onSurface} fontWeight="bold">
+        아직 참여자가 없는 랭킹입니다 :)
+      </Text>
+      <Text size={16} color={themeContext.onSurface}>
+        인증하고 랭킹 1등 자리를 차지해보아요!! 🏆
+      </Text>
+    </NonDataWrapper>
+  );
+};
+
+const Wrapper = styled(FlexBox)`
+  flex-grow: 1;
+`;
+
+const NonDataWrapper = styled(FlexBox)`
+  ${({ theme }) => css`
+    flex-grow: 1;
+    height: 100%;
+    border-radius: 1rem;
+    background-color: ${theme.surface};
+    padding: 0.5rem 1rem;
+  `}
+`;
