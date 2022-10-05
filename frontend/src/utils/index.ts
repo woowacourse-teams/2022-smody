86af5acafb34c23627124660d77608ecccb1b3f8
2019015414 <2019015414@hanyang.ac.kr>
2022-10-05 14:06:42 +0900

    feat: 기간 표시 UI 변경

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -86,3 +86,16 @@ export const dateYMDFormatParsing = (dateString: string) => {
   const { year, month, date } = parseTime(dateString);
   return `${year}.${month}.${date}`;
 };
+
+export const getWeekNumber = (dateString: string) => {
+  const dateFrom = new Date(Date.parse(dateString));
+  // 해당 날짜 (일)
+  const currentDate = dateFrom.getDate();
+  // 이번 달 1일로 지정
+  const startOfMonth = new Date(dateFrom.setDate(1));
+  // 이번 달 1일이 무슨 요일인지 확인
+  const weekDay = startOfMonth.getDay(); // 0: Sun ~ 6: Sat
+
+  // ((요일 - 1) + 해당 날짜) / 7일로 나누기 = N 주차
+  return parseInt(`${(weekDay - 1 + currentDate) / 7}`) + 1;
+};
