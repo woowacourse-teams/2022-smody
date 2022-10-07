5a55ce0089df76d135ea07c088d9d1c1a0174417
ronci <jyj9647@naver.com>
2022-10-07 17:17:14 +0900

    feat: (fe) 챌린저 리스트 디자인 수정 (#515)
    * feat: 챌린저 리스트 margin 수정
    
    * feat: pwa 버전 올리기

--- frontend/src/components/ChallengerList/index.tsx
+++ frontend/src/components/ChallengerList/index.tsx
@@ -26,7 +26,6 @@ export const ChallengerList = ({ challengeId }: ChallengerListProps) => {
           <Challenger {...challenger} />
         </li>
       ))}
-      <div style={{ height: '130px' }}></div>
     </FlexBox>
   );
 };
