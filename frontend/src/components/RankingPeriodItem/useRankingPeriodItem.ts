0ed1b44866bb910595799df45d9e9c5b28595051
2019015414 <2019015414@hanyang.ac.kr>
2022-10-07 18:19:02 +0900

    feat: 랭킹 기간 리스트 표기 수정

--- frontend/src/components/RankingPeriodItem/useRankingPeriodItem.ts
+++ frontend/src/components/RankingPeriodItem/useRankingPeriodItem.ts
@@ -6,7 +6,7 @@ import { RANKING_DURATION } from 'constants/domain';
 const useRankingPeriodItem = ({ startDate, duration }: UseRankingPeriodItemProps) => {
   const startDateString = dateYMDFormatParsing(startDate);
   const endDateString = dateYMDFormatParsing(
-    String(addDays(new Date(Date.parse(startDate)), RANKING_DURATION[duration])),
+    String(addDays(new Date(Date.parse(startDate)), RANKING_DURATION[duration] - 1)),
   );
   const detailDateString = `${startDateString} ~ ${endDateString}`;
 
