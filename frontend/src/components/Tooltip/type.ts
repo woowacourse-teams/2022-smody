c844f1262b3f0489bec21feb704b5d9ad901a459
wonsss <bigdatajang@gmail.com>
2022-10-13 00:38:15 +0900

    feat: 툴팁 prop에 xPosition, yPosition 추가

--- frontend/src/components/Tooltip/type.ts
+++ frontend/src/components/Tooltip/type.ts
@@ -2,4 +2,6 @@ export type TooltipProps = {
   title: string;
   isOpenTooltip: boolean;
   toggleTooltip: () => void;
+  xPosition?: 'left' | 'middle' | 'right';
+  yPosition?: 'top' | 'bottom';
 };
