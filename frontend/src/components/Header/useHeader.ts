5377a1d8f04a63d7a0299fbded296676afacb40e
woose28 <dldntmdsla@naver.com>
2022-10-14 20:14:02 +0900

    feat: 특정 페이지에서만 Header의 스크롤 애니메이션이 실행되도록 조건 추가
    - 피드 상세 페이지에서만 애니메이션이 실행되도록 구현

--- frontend/src/components/Header/useHeader.ts
+++ frontend/src/components/Header/useHeader.ts
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { isDarkState } from 'recoil/darkMode/atoms';
 
@@ -8,19 +9,27 @@ export const useHeader = () => {
   const wrapperRef = useRef<HTMLDivElement>(null);
   const [isDark, setIsDark] = useRecoilState(isDarkState);
   const scrollDirection = useScrollDirection();
+  const { pathname } = useLocation();
 
   useEffect(() => {
+    if (isNotNeedScrollAnimation) {
+      return;
+    }
+
     if (scrollDirection === 'up') {
       wrapperRef.current!.style.transform = 'translateY(0rem)';
 
       return;
     }
+
     if (scrollDirection === 'down') {
       wrapperRef.current!.style.transform = 'translateY(-3rem)';
 
       return;
     }
   }, [scrollDirection]);
+
+  const isNotNeedScrollAnimation = !pathname.startsWith('/feed/detail');
 
   const handleDarkToggle = () => {
     localStorage.setItem('isDark', JSON.stringify(!isDark));
