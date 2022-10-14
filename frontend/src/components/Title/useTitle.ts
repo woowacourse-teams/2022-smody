1ec638336a003e2e42ce8a9384243da66177c591
woose28 <dldntmdsla@naver.com>
2022-10-14 20:34:47 +0900

    fix: 인증 기록 보기에의 Title 컴포넌트 배경색 변경

--- frontend/src/components/Title/useTitle.ts
+++ frontend/src/components/Title/useTitle.ts
@@ -2,9 +2,14 @@ import { UseTitleProps } from './type';
 import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import useMatchPath from 'hooks/useMatchPath';
 import useScrollDirection from 'hooks/useScrollDirection';
+import useThemeContext from 'hooks/useThemeContext';
+
+import { CLIENT_PATH } from 'constants/path';
 
 const useTitle = ({ linkTo }: UseTitleProps) => {
+  const themeContext = useThemeContext();
   const navigate = useNavigate();
   const backToPreviousPage = () => {
     if (linkTo) {
@@ -16,6 +21,9 @@ const useTitle = ({ linkTo }: UseTitleProps) => {
   };
   const TitleRef = useRef<HTMLDivElement>(null);
   const scrollDirection = useScrollDirection();
+  const getPathMatchColor = useMatchPath(themeContext.secondary, themeContext.background);
+
+  const bgColor = getPathMatchColor([CLIENT_PATH.CERT, CLIENT_PATH.CYCLE_DETAIL]);
 
   useEffect(() => {
     if (!TitleRef || !TitleRef.current) {
@@ -33,7 +41,7 @@ const useTitle = ({ linkTo }: UseTitleProps) => {
     }
   }, [scrollDirection]);
 
-  return { backToPreviousPage, TitleRef };
+  return { backToPreviousPage, TitleRef, themeContext, bgColor };
 };
 
 export default useTitle;
