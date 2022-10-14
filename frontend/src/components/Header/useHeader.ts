15e259d6837d6db1a66161d12d2e374d36a36332
woose28 <dldntmdsla@naver.com>
2022-10-14 16:59:37 +0900

    fix: 알림 목록이 Title에 가려지는 문제 해결
    - Title 영역의 z-index를 Header 영역보다 낮게 설정
    - Header 영역도 스크롤 시 위로 이동하도록 구현

--- frontend/src/components/Header/useHeader.ts
+++ frontend/src/components/Header/useHeader.ts
@@ -1,8 +1,26 @@
+import { useRef, useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import { isDarkState } from 'recoil/darkMode/atoms';
 
+import useScrollDirection from 'hooks/useScrollDirection';
+
 export const useHeader = () => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
   const [isDark, setIsDark] = useRecoilState(isDarkState);
+  const scrollDirection = useScrollDirection();
+
+  useEffect(() => {
+    if (scrollDirection === 'up') {
+      wrapperRef.current!.style.top = '0rem';
+
+      return;
+    }
+    if (scrollDirection === 'down') {
+      wrapperRef.current!.style.top = '-3rem';
+
+      return;
+    }
+  }, [scrollDirection]);
 
   const handleDarkToggle = () => {
     localStorage.setItem('isDark', JSON.stringify(!isDark));
@@ -10,6 +28,7 @@ export const useHeader = () => {
   };
 
   return {
+    wrapperRef,
     isDark,
     handleDarkToggle,
   };
