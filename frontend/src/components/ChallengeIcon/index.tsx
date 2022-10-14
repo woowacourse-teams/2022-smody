0a299993b2a15e7d6e0a78b13f08fce9217cea27
Marco <bigdatajang@gmail.com>
2022-10-14 18:48:35 +0900

    feat: CSS sprite를 활용하여 우테코 프로젝트 아이콘을 ChallengeIcon 지정
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- frontend/src/components/ChallengeIcon/index.tsx
+++ frontend/src/components/ChallengeIcon/index.tsx
@@ -1,14 +1,41 @@
+import { ChallengeIconProps, ImgIconListIndexSignature } from './type';
+import projectIcons from 'assets/project_icons.png';
 import styled, { css } from 'styled-components';
 
 import { FlexBox } from 'components';
-import { ChallengeIconProps } from 'components/ChallengeIcon/type';
 
 import { emojiList } from 'constants/style';
+
+const ratio = 0.7;
+
+const IMG_SIZES = {
+  small: 0.297 * ratio,
+  medium: 0.456 * ratio,
+  large: 0.6 * ratio,
+};
 
 const SIZES = {
   small: { width: '2.6rem', fontSize: '1.6rem' },
   medium: { width: '4rem', fontSize: '2.2rem' },
   large: { width: '7.5rem', fontSize: '4.375rem' },
+};
+
+const ImgIconList: ImgIconListIndexSignature = {
+  1: 'f12',
+  2: 'checkmate',
+  3: 'dallok',
+  4: 'ducks',
+  5: 'gongcheck',
+  6: 'gongseek',
+  7: 'jupjup',
+  8: 'kkogkkog',
+  9: 'moamoa',
+  10: 'momo',
+  11: 'morak',
+  12: 'smody',
+  13: 'sokdaksokdak',
+  14: 'teatime',
+  15: 'ternoko',
 };
 
 export const ChallengeIcon = ({
@@ -17,6 +44,17 @@ export const ChallengeIcon = ({
   emojiIndex,
   challengeId,
 }: ChallengeIconProps) => {
+  if (
+    typeof challengeId !== 'undefined' &&
+    Object.keys(ImgIconList).includes(String(challengeId))
+  ) {
+    return (
+      <ProjectIcon size={size} justifyContent="center" alignItems="center">
+        <Icon className={ImgIconList[challengeId]} />
+      </ProjectIcon>
+    );
+  }
+
   return (
     <Wrapper size={size} bgColor={bgColor} justifyContent="center" alignItems="center">
       {emojiList[emojiIndex]}
@@ -24,7 +62,7 @@ export const ChallengeIcon = ({
   );
 };
 
-export const Wrapper = styled(FlexBox)<Pick<ChallengeIconProps, 'size' | 'bgColor'>>`
+const Wrapper = styled(FlexBox)<Pick<ChallengeIconProps, 'size' | 'bgColor'>>`
   ${({ size, bgColor }) => css`
     min-width: ${SIZES[size].width};
     max-width: ${SIZES[size].width};
@@ -33,5 +71,105 @@ export const Wrapper = styled(FlexBox)<Pick<ChallengeIconProps, 'size' | 'bgColo
     border-radius: 50%;
     background-color: ${bgColor};
     font-size: ${SIZES[size].fontSize};
+  `}
+`;
+
+const Icon = styled.div`
+  background-size: cover;
+`;
+
+const ProjectIcon = styled(FlexBox)<Pick<ChallengeIconProps, 'size'>>`
+  ${({ size }) => css`
+    min-width: ${SIZES[size].width};
+    max-width: ${SIZES[size].width};
+    min-height: ${SIZES[size].width};
+    max-height: ${SIZES[size].width};
+    border-radius: 50%;
+
+    .f12,
+    .checkmate,
+    .dallok,
+    .ducks,
+    .gongcheck,
+    .gongseek,
+    .jupjup,
+    .kkogkkog,
+    .moamoa,
+    .momo,
+    .morak,
+    .smody,
+    .sokdaksokdak,
+    .teatime,
+    .ternoko {
+      display: inline-block;
+      background: url(${projectIcons}) no-repeat;
+      overflow: hidden;
+
+      width: 200px;
+      height: 200px;
+      border-radius: 50%;
+      zoom: ${IMG_SIZES[size]};
+    }
+
+    .f12 {
+      background-position: 0 0;
+      background-color: rgb(254, 232, 232);
+    }
+    .checkmate {
+      background-position: -200px 0;
+      background-color: #fcead4;
+    }
+    .dallok {
+      background-position: -400px 0;
+      background-color: #fff5db;
+    }
+    .ducks {
+      background-position: 0 -200px;
+      background-color: #ffffff;
+    }
+    .gongcheck {
+      background-position: -200px -200px;
+      background-color: #e1f3fe;
+    }
+    .gongseek {
+      background-position: -400px -200px;
+      background-color: #f6f1ff;
+    }
+    .jupjup {
+      background-position: 0 -400px;
+      background-color: #ffe6c1;
+    }
+    .kkogkkog {
+      background-position: -200px -400px;
+      background-color: #fff5ef;
+    }
+    .moamoa {
+      background-position: -400px -400px;
+      background-color: #d0ccff;
+    }
+    .momo {
+      background-position: 0 -600px;
+      background-color: #ffffff;
+    }
+    .morak {
+      background-position: -200px -600px;
+      background-color: #f6f1ff;
+    }
+    .smody {
+      background-position: -400px -600px;
+      background-color: #ffffff;
+    }
+    .sokdaksokdak {
+      background-position: 0 -800px;
+      background-color: #ceffe6;
+    }
+    .teatime {
+      background-position: -200px -800px;
+      background-color: #ffffff;
+    }
+    .ternoko {
+      background-position: -400px -800px;
+      background-color: rgb(255, 232, 232);
+    }
   `}
 `;
--- frontend/src/components/ChallengeIcon/type.ts
+++ frontend/src/components/ChallengeIcon/type.ts
@@ -1,6 +1,13 @@
+import { PickType } from 'smody-library';
+import { Challenge } from 'types/challenge';
+
 export type ChallengeIconProps = {
   size: 'small' | 'medium' | 'large';
   bgColor: string;
   emojiIndex: number;
   challengeId?: number;
+};
+
+export type ImgIconListIndexSignature = {
+  [key: PickType<Challenge, 'challengeId'>]: string;
 };
