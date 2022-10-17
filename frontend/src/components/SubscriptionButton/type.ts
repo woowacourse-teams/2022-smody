f2b7a03978ec9594a0431e77a6aa28ee66e36e52
2019015414 <2019015414@hanyang.ac.kr>
2022-10-17 13:58:58 +0900

    refactor: SubscriptionButton 리팩터링

--- frontend/src/components/SubscriptionButton/type.ts
+++ frontend/src/components/SubscriptionButton/type.ts
@@ -1,3 +1,7 @@
 export type SubscriptionButtonProps = {
   updateIsSubscribed: (updatedIsSubscribed: boolean) => void;
 };
+
+export type UseSubscriptionButtonProps = SubscriptionButtonProps;
+
+export type InfoAboutSubscribeProps = { isAbleSubscribe: boolean };
