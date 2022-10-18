972b29105e7529b89d43ae6d5a889dd325e2792b
2019015414 <2019015414@hanyang.ac.kr>
2022-10-18 13:31:38 +0900

    feat: 랜덤 챌린지 페이지와 인기 챌린지 페이지에 각 각 title 추가

--- frontend/src/pages/PopularChallengePage/index.tsx
+++ frontend/src/pages/PopularChallengePage/index.tsx
@@ -1,15 +1,31 @@
 import { usePopularChallengePage } from './usePopularChallengePage';
+import styled from 'styled-components';
 
-import { ChallengeList } from 'components';
+import useThemeContext from 'hooks/useThemeContext';
+
+import { ChallengeList, FlexBox, Text } from 'components';
 
 const PopularChallengePage = () => {
+  const themeContext = useThemeContext();
   const { challengeInfiniteData } = usePopularChallengePage();
 
   if (typeof challengeInfiniteData === 'undefined') {
     return null;
   }
 
-  return <ChallengeList challengeInfiniteData={challengeInfiniteData.pages} />;
+  return (
+    <FlexBox flexDirection="column">
+      <TitleText color={themeContext.onBackground} size={20} fontWeight="bold">
+        🔥 가장 많이 참여하고 있는 챌린지 🔥
+      </TitleText>
+      <ChallengeList challengeInfiniteData={challengeInfiniteData.pages} />;
+    </FlexBox>
+  );
 };
 
 export default PopularChallengePage;
+
+const TitleText = styled(Text)`
+  text-align: center;
+  margin-bottom: 1.5rem;
+`;
--- frontend/src/pages/RandomChallengePage/index.tsx
+++ frontend/src/pages/RandomChallengePage/index.tsx
@@ -1,15 +1,31 @@
 import { useRandomChallengePage } from './useRandomChallengePage';
+import styled from 'styled-components';
 
-import { ChallengeList } from 'components';
+import useThemeContext from 'hooks/useThemeContext';
+
+import { ChallengeList, FlexBox, Text } from 'components';
 
 const RandomChallengePage = () => {
+  const themeContext = useThemeContext();
   const { challengeInfiniteData } = useRandomChallengePage();
 
   if (typeof challengeInfiniteData === 'undefined') {
     return null;
   }
 
-  return <ChallengeList challengeInfiniteData={challengeInfiniteData.pages} />;
+  return (
+    <FlexBox flexDirection="column">
+      <TitleText color={themeContext.onBackground} size={20} fontWeight="bold">
+        ❔ 이런 챌린지는 어때요 ❔
+      </TitleText>
+      <ChallengeList challengeInfiniteData={challengeInfiniteData.pages} />;
+    </FlexBox>
+  );
 };
 
 export default RandomChallengePage;
+
+const TitleText = styled(Text)`
+  text-align: center;
+  margin-bottom: 1.5rem;
+`;
