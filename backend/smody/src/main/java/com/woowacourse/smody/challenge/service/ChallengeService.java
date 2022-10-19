406bfdcbb48db64c445e377cb056d4ccae8e347d
jojogreen91 <iawbg13@gmail.com>
2022-10-20 00:18:33 +0900

    refactor: 챌린지 추천 로직 수정

--- backend/smody/src/main/java/com/woowacourse/smody/challenge/service/ChallengeService.java
+++ backend/smody/src/main/java/com/woowacourse/smody/challenge/service/ChallengeService.java
@@ -5,6 +5,7 @@ import com.woowacourse.smody.challenge.repository.ChallengeRepository;
 import com.woowacourse.smody.db_support.PagingParams;
 import com.woowacourse.smody.exception.BusinessException;
 import com.woowacourse.smody.exception.ExceptionData;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -51,6 +52,20 @@ public class ChallengeService {
     private void validateDuplicatedName(String name) {
         if (challengeRepository.findByName(name).isPresent()) {
             throw new BusinessException(ExceptionData.DUPLICATE_NAME);
+        }
+    }
+
+    public List<Challenge> findRandomChallenges(Integer size) {
+        List<Long> allIds = challengeRepository.findAllIds();
+        validateSize(allIds, size);
+        Collections.shuffle(allIds);
+        List<Long> randomIds = allIds.subList(0, size);
+        return challengeRepository.findAllByIdIn(randomIds);
+    }
+
+    private void validateSize(List<Long> allIds, Integer size) {
+        if (allIds.size() < size) {
+            throw new BusinessException(ExceptionData.OUT_OF_BOUND_RANDOM_SIZE);
         }
     }
 }
