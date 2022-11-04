3cdb17d040c2965ff5bcb5587aa5bf4c7bc8882b
bcc0830 <bcc0830@bithub.com>
2022-11-05 03:31:34 +0900

    test : 쿼리문 변경

--- backend/smody/src/main/java/com/woowacourse/smody/record/repository/RecordRepository.java
+++ backend/smody/src/main/java/com/woowacourse/smody/record/repository/RecordRepository.java
@@ -18,14 +18,12 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
 
     Optional<Record> findByMemberAndAndChallenge(Member member, Challenge challenge);
 
-    @Query(value = "select r.challenge_id, count(r.record_id) from record r where (select r.record_id from record r " +
-            "where r.challenge_id in :challenges and r.dead_line >= :startTime and r.is_success=false " +
-            "order by r.record_id limit 1) <= r.record_id and r.challenge_id in :challenges and r.dead_line >= :startTime and r.is_success=false " +
-            "group by r.challenge_id", nativeQuery = true)
+    @Query(value = "select r.challenge_id, count(*) from record r" +
+            " where r.challenge_id in :challenges and r.dead_line >= :startTIme group by r.challenge_id", nativeQuery = true)
     List<Long []> countChallengersMultipleChallenge(@Param("challenges") List<Challenge> challenges,
                                              @Param("startTime") LocalDateTime startTime);
 
-    @Query(value = "select r.challenge_id from record r where r.member_id = :member and r.challenge_id in :challenges and r.dead_line >= :startTime and r.is_success = false", nativeQuery = true)
+    @Query(value = "select r.challenge_id from record r where r.member_id = :member and r.challenge_id in :challenges and r.dead_line >= :startTime", nativeQuery = true)
     List<Long> isInProgressMultipleChallenge(@Param("member") Member member, @Param("challenges") List<Challenge> challenges,
                                         @Param("startTime") LocalDateTime startTime);
 
