da0de39b80e50d3b622fd6d6ed7b4e6d796c0a89
wonsss <bigdatajang@gmail.com>
2022-11-05 12:22:53 +0900

    fix: Jest에서 svg 처리되지 않는 문제 해결
    transform에서 jest-transform-svg 플러그인을 통해 svg 처리하고 있는데, moduleNameMapper에서도 svg 처리를 이중으로 하고 있어 문제 발생했던 것으로 보임.
    
    moduleNameMapper에서 처리 확장자 중 svg를 삭제함으로써 문제 해결

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -10,13 +10,11 @@ const config: Config.InitialOptions = {
   },
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/**/*.test.(js|jsx|ts|tsx)'],
-  // transformIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleDirectories: ['node_modules', 'src'],
 
   moduleNameMapper: {
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/mocks/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/mocks/fileMock.js',
   },
   rootDir: './src',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
