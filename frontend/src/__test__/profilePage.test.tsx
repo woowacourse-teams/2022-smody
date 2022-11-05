a466f2c1b1954a1f44bbd71ff3557707a19ad0d4
wonsss <bigdatajang@gmail.com>
2022-11-05 12:25:42 +0900

    fix: jest.isolateModules() 제거 및 테스트 코드 정상 작동하도록 수정

--- frontend/src/__test__/profilePage.test.tsx
+++ frontend/src/__test__/profilePage.test.tsx
@@ -12,34 +12,26 @@ describe('프로필 페이지 테스트', () => {
     renderWithProviders(<ProfilePage />);
   });
 
-  test('[useGetMyInfo] query 응답에 따라 나의 프로필 정보가 렌더링되는지 확인한다.', () => {
-    jest.isolateModules(async () => {
-      const nickname = await waitFor(() => screen.getByLabelText('닉네임'));
-      await waitFor(() => expect(nickname).toHaveTextContent('테스트닉네임'));
+  test('[useGetMyInfo] query 응답에 따라 나의 프로필 정보가 렌더링되는지 확인한다.', async () => {
+    const nickname = await waitFor(() => screen.getByLabelText('닉네임'));
+    await waitFor(() => expect(nickname).toHaveTextContent('테스트닉네임'));
 
-      const introduction = await waitFor(() => screen.getByLabelText('자기소개'));
-      await waitFor(() => expect(introduction).toHaveTextContent('테스트 자기소개'));
-    });
+    const introduction = await waitFor(() => screen.getByLabelText('자기소개'));
+    await waitFor(() => expect(introduction).toHaveTextContent('테스트 자기소개'));
   });
 
-  test('[useGetMyCyclesStat] query 응답에 따라 나의 챌린지 통계가 렌더링되는지 확인한다.', () => {
-    jest.isolateModules(async () => {
-      const totalCount = await waitFor(() => screen.getByLabelText('시도한 챌린지 횟수'));
-      await waitFor(() => expect(totalCount).toHaveTextContent('35'));
+  test('[useGetMyCyclesStat] query 응답에 따라 나의 챌린지 통계가 렌더링되는지 확인한다.', async () => {
+    const totalCount = await waitFor(() => screen.getByLabelText('시도한 챌린지 횟수'));
+    await waitFor(() => expect(totalCount).toHaveTextContent('35'));
 
-      const successCount = await waitFor(() =>
-        screen.getByLabelText('성공한 챌린지 횟수'),
-      );
-      await waitFor(() => expect(successCount).toHaveTextContent('5'));
-    });
+    const successCount = await waitFor(() => screen.getByLabelText('성공한 챌린지 횟수'));
+    await waitFor(() => expect(successCount).toHaveTextContent('5'));
   });
 
-  test('[useGetMySuccessChallenges] query 응답에 따라 성공한 챌린지 목록이 렌더링되는지 확인한다.', () => {
-    jest.isolateModules(async () => {
-      const successChallenge = await waitFor(
-        () => screen.getAllByLabelText('성공한 챌린지 이름')[0],
-      );
-      await waitFor(() => expect(successChallenge).toHaveTextContent('미라클 모닝'));
-    });
+  test('[useGetMySuccessChallenges] query 응답에 따라 성공한 챌린지 목록이 렌더링되는지 확인한다.', async () => {
+    const successChallenge = await waitFor(
+      () => screen.getAllByLabelText('성공한 챌린지 이름')[0],
+    );
+    await waitFor(() => expect(successChallenge).toHaveTextContent('미라클 모닝'));
   });
 });
--- frontend/src/__test__/searchPage.test.tsx
+++ frontend/src/__test__/searchPage.test.tsx
@@ -1,4 +1,3 @@
-import { act } from 'react-dom/test-utils';
 import { renderWithProviders } from 'utils/testUtils';
 
 import { screen, waitFor } from '@testing-library/react';
@@ -10,12 +9,10 @@ describe('검색 페이지 테스트', () => {
     renderWithProviders(<SearchPage />);
   });
 
-  test('[useGetAllChallenges] query 응답에 따라 모든 챌린지가 렌더링되는지 확인한다.', () => {
-    jest.isolateModules(async () => {
-      const challengeName = await waitFor(
-        () => screen.getAllByLabelText('challenge-name')[0],
-      );
-      await waitFor(() => expect(challengeName).toHaveTextContent('미라클 모닝'));
-    });
+  test('[useGetAllChallenges] query 응답에 따라 모든 챌린지가 렌더링되는지 확인한다.', async () => {
+    const challengeName = await waitFor(
+      () => screen.getAllByLabelText('challenge-name')[0],
+    );
+    await waitFor(() => expect(challengeName).toHaveTextContent('미라클 모닝'));
   });
 });
