7d9fe655534c9f94c16ba864a95f580661a98695
wonsss <bigdatajang@gmail.com>
2022-11-05 14:23:20 +0900

    feat: jest moduleNameMapper 설정에서사용하는 확장자만 남김

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -13,8 +13,7 @@ const config: Config.InitialOptions = {
   moduleDirectories: ['node_modules', 'src'],
 
   moduleNameMapper: {
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/mocks/fileMock.js',
+    '\\.(jpg|ico|jpeg|png|gif|webp)$': '<rootDir>/mocks/fileMock.js',
   },
   rootDir: './src',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
