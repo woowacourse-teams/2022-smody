d06f4e5eab40d705958548610d83c3bd40fa7bed
Marco <bigdatajang@gmail.com>
2022-11-08 17:11:51 +0900

    refactor: renderWithProviders 간소화

--- frontend/src/__test__/renderWithProviders.tsx
+++ frontend/src/__test__/renderWithProviders.tsx
@@ -1,6 +1,6 @@
 import { generateQueryClient } from 'queryClient';
 import { ReactElement } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { MemoryRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
@@ -16,18 +16,14 @@ const generateTestQueryClient = () => {
   return client;
 };
 
-export function renderWithProviders(
-  ui: ReactElement,
-  client?: QueryClient,
-): RenderResult {
-  const queryClient = client ?? generateTestQueryClient();
+export const renderWithProviders = (Component: ReactElement): RenderResult => {
   return render(
     <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <ThemeProvider theme={darkTheme}>{ui}</ThemeProvider>
-        </MemoryRouter>
+      <QueryClientProvider client={generateTestQueryClient()}>
+        <ThemeProvider theme={darkTheme}>
+          <MemoryRouter>{Component}</MemoryRouter>
+        </ThemeProvider>
       </QueryClientProvider>
     </RecoilRoot>,
   );
-}
+};
